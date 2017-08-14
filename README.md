3598335987a6a0fc8f1e729be2318b9275bd4b78
a.atalla <a.3talla@gmail.com>
2017-08-14 14:07:38 +0200

    update readme, apply standard.js

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-## TODO
+## Booktips Task Server code
+- clone the repo and install dependencies then run the server
+
+```
+git clone git@github.com:a-atalla/Booktips-task-server.git
+cd Booktips-task-client
+npm install
+npm start
+```
+
+- run the [client](https://github.com/a-atalla/Booktips-task-client) and check on the browser
--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-const express = require('express');
-const logger = require('morgan');
+const express = require('express')
+const logger = require('morgan')
 
-const google = require('googleapis');
-const books = google.books('v1');
+const google = require('googleapis')
+const books = google.books('v1')
 
-
-
-///////////////////////////////////////////////////
 // Initializing the App
-const app = express();
-app.use(logger('dev'));
-
+const app = express()
+app.use(logger('dev'))
 
 // Enable CORS from client-side
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next()
+})
 
-
-///////////////////////////////////////////////////////////////
 // Routes
-app.get('/', (req, res)=>{
-  res.json({"state": "Welcome to Google Search server"});
-});
+app.get('/', (req, res) => {
+  res.json({'state': 'Welcome to Google Search server'})
+})
 
-app.get('/search/:title', (req, res)=> {
+app.get('/search/:title', (req, res) => {
   let bookTitle = req.params.title
   let startIndex = req.query.start
   let maxResults = req.query.max
@@ -37,16 +31,16 @@ app.get('/search/:title', (req, res)=> {
     q: bookTitle,
     startIndex,
     maxResults
-    }, (err, result) => {
-      if (err) {
-        res.json(err)
-      } else {
-        res.json(result)
-      }
-  });
+  }, (err, result) => {
+    if (err) {
+      res.json(err)
+    } else {
+      res.json(result)
+    }
+  })
 })
 
-app.get('/books/:bookId', (req, res)=> {
+app.get('/books/:bookId', (req, res) => {
   let bookId = req.params.bookId
   books.volumes.get({volumeId: bookId}, function (err, result) {
     if (err) {
@@ -54,12 +48,10 @@ app.get('/books/:bookId', (req, res)=> {
     } else {
       res.json(result)
     }
-  });
+  })
 })
 
-
-///////////////////////////////////////////////////////////////
 // Starting the server
-app.listen(3030, ()=>{
-  console.log("Server is running at http://127.0.0.1:3030")
+app.listen(3030, () => {
+  console.log('Server is running at http://127.0.0.1:3030')
 })
